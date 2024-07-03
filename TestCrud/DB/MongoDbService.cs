034f04d6255b54db1270fafc57f26a9fff188584
Rahat Khan Pathan <85352119+Rahat-Khan-Pathan@users.noreply.github.com>
2024-07-03 14:36:26 +0600

    Add files via upload

---
+++ TestCrud/DB/MongoDbService.cs
@@ -0,0 +1,21 @@
+﻿using CrudApp.Entities;
+using MongoDB.Driver;
+
+namespace CrudApp.DB
+{
+    public class MongoDbService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase? _database;
+        public MongoDbService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+            
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+        public IMongoDatabase? Database => _database;
+    }
+}
---
+++ TestCrud/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CrudApp.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("name"), BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+        [BsonElement("createdAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime? CreatedAt { get; set; }
+        [BsonElement("updatedAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
